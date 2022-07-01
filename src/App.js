43d08132ff9da74248ba3e7f42a2e6c2c4630d22
Brian Levine <blevine@reverb.com>
2022-07-01 09:54:55 -0500

    Ok it's the start, everything is going to be ok

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import guenter from './guenter-happy.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={guenter} className="guenter" alt="logo" />
+        <p className="car">
+          🏎️
+        </p>
+        <p className="road">
+          -------------------------------------------------------------------------------------
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
